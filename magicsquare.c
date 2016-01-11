4362bc2d247d25fe083612adb0d9df700bbd4a57
sasidaran22 <kumar.sasidaran@gmail.com>
2016-01-11 07:26:01 +0530

    Create magicsquare.c

---
+++ magicsquare.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+void generatematrix(int n)
+{ 	int i,j,size,num,matrix[50][50];
+  	size=n*n;
+  	i=0;j=n/2;//initial element position
+  	for(num=1;num<=size;)
+  	{
+  		if (i==-1&&j==n) 
+            {
+                j=n-1;
+                i=1;
+            }
+        else
+            {
+                if (j==n)
+                    j=0;
+                if (i<0)
+                    i=n-1;
+            }
+        if (matrix[i][j])
+            {
+                j=j-1;
+                i=i+2;
+                continue;
+            }
+        else
+            {
+                matrix[i][j] = num; 
+                num++;
+            }
+            j++; 
+            i--;
+  	}
+  	for(i=0;i<n;i++)
+	{	for(j=0;j<n;j++)
+		{
+			printf("%d\t",matrix[i][j]);
+		}
+		printf("\n");
+	}
+  	
+        
+}
+  	
+
+int main(void) 
+{
+	int i,j,n,mat[n][n];
+	printf("Enter the order of the square matrix(only odd number)\n");
+	scanf("%d",&n);
+	if(n%2==0)
+		{printf("Matrix cannot be generated\n");
+		}
+	
+	generatematrix(n);
+	return 0;
+}
+#include <stdio.h>
+void generatematrix(int n)
+{ 	int i,j,size,num,matrix[50][50];
+  	size=n*n;
+  	i=0;j=n/2;//initial element position
+  	for(num=1;num<=size;)
+  	{
+  		if (i==-1&&j==n) 
+            {
+                j=n-1;
+                i=1;
+            }
+        else
+            {
+                if (j==n)
+                    j=0;
+                if (i<0)
+                    i=n-1;
+            }
+        if (matrix[i][j])
+            {
+                j=j-1;
+                i=i+2;
+                continue;
+            }
+        else
+            {
+                matrix[i][j] = num; 
+                num++;
+            }
+            j++; 
+            i--;
+  	}
+  	for(i=0;i<n;i++)
+	{	for(j=0;j<n;j++)
+		{
+			printf("%d\t",matrix[i][j]);
+		}
+		printf("\n");
+	}
+  	
+        
+}
+  	
+
+int main() 
+{
+	int i,j,n,mat[n][n];
+	printf("Enter the order of the square matrix(only odd number)\n");
+	scanf("%d",&n);
+	if(n%2==0)
+		{printf("Matrix cannot be generated\n");
+		}
+	else
+  	generatematrix(n);
+	return 0;
+}
