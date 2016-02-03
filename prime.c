b5686899abffdb3fe5398b6a9c368e09ba85c6e3
sasidaran22 <kumar.sasidaran@gmail.com>
2016-02-03 19:37:50 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -7,25 +7,25 @@ int main()
     printf("Enter the number\n");
     scanf("%d",&num);
     if(num%2==0)
-        {
-            num=num+1;
-        }
+    {
+        num=num+1;
+    }
     else
         num+=2;
 
     while(1)
-	    {
-		      if(nextprime(num))
-			        break;
-		      num+=2;
-	    }
-	printf("The next prime number is %d\n",num);
-	return 0;
+    {
+	if(nextprime(num))
+	    break;
+	num+=2;
+    }
+    printf("The next prime number is %d\n",num);
+    return 0;
 }
 int nextprime(int n)
 {
     int i;
-	for(i=2;i<=n/2;i++)
+	for(i=3;i<=n/2;i=i+2)
 	{
 		if(n%i==0)
 			return 0;
