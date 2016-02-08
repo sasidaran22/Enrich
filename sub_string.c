422e4fe2338ccec0836181a80c7ad69d747bc9c7
sasidaran22 <kumar.sasidaran@gmail.com>
2016-02-08 16:34:39 +0530

    Update sub_string.c

--- sub_string.c
+++ sub_string.c
@@ -1,16 +1,35 @@
 #include<stdio.h>
-#include<string.h>
+
 void compare(char* temp,char* res)
 {
-    if(strlen(temp)>strlen(res))
+    if(length(temp)>length(res))
     {
-        strcpy(res,temp);
+        copy(res,temp);
     }
+}
+int length(char* str)
+{
+    int i,len=0;
+    for(i=0;str[i]!='\0';i++)
+    {
+        len++;
+    }
+    return len;
+}
+void copy(char* a,char* b)
+{
+    int i;
+    for(i=0;b[i]!='\0';i++)
+    {
+        a[i]=b[i];
+    }
+    a[i+1]='\0';
 }
 int main()
 {
     char str[30],res[30]={},temp[30];
     int i,j,k=1,count=1;
+    printf("Enter the string\n");
     scanf("%s",str);
     printf("The string is %s\n\n",str);
     for(i=1;str[i]!='\0';i++)
@@ -45,7 +64,8 @@ int main()
         }
     }
     printf("The longest sub-string without repitition is %s\n",res);
-    printf("The length of the string is %d\n",strlen(res));
+    printf("The length of the string is %d\n",length(res));
 
     return 0;
 }
+
