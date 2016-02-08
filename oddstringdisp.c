041953e8309bfc4de1f924575233eddb0bccb990
sasidaran22 <kumar.sasidaran@gmail.com>
2016-02-08 20:48:54 +0530

    Update oddstringdisp.c

--- oddstringdisp.c
+++ oddstringdisp.c
@@ -36,16 +36,14 @@ int main()
             }
         }
     }
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                printf("%c  ",res[i][j]);
-            }
-            printf("\n");
-        }
+        
+    for(i=0;i<n;i++)
+    {
+         for(j=0;j<n;j++)
+         {
+            printf("%c  ",res[i][j]);
+         }
+         printf("\n");
+    }
     return 0;
-
-
-
 }
